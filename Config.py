4a165d4f510bcdecddc2b0b9dce61939e07f4c86
Ehsan Aghaei <46252665+ehsanaghaei@users.noreply.github.com>
2024-01-09 12:08:11 -0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,6 +3,6 @@ import os
 
 class Config:
     load_tokenizer = True
-    path_VO_data = "/media/ea/SSD2/DB/Dropbox/BERT_Torch/data/VO2Func"
+    path_VO_data = "./data/VO2Func"
     train = False
     eval = False
